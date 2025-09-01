5415c5fb3502e5c37d3955b19f6c8434a87c7454
carlopav <carlopav@gmail.com>
2025-09-01 16:12:50 +0200

    Hello world script for handling Ifc linked Point Clouds in Bonsai

---
+++ IFC_StoredPointCloudUI.py
@@ -0,0 +1,64 @@
+import bpy
+from bl_ui.generic_ui_list import draw_ui_list
+
+from bonsai import tool
+
+
+class MyPropGroup(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty()
+
+
+class IfcPointCloudPanel(bpy.types.Panel):
+    bl_label = "Ifc Point Cloud Reference"
+    bl_idname = "IfcPointCloudPanel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "IfcPC"
+    
+    @staticmethod
+    def find_pcs():
+        model = tool.Ifc.get()
+        pcs=[]
+        for ref in model.by_type("IfcDocumentReference"):
+            if ref.Location and ref.Location[-3:] in ("e57","ply"):
+                pcs.append(ref)
+        return pcs
+    
+    def draw(self, context):
+        layout = self.layout
+        draw_ui_list(
+            layout,
+            context,
+            list_path="scene.ifc_point_cloud_list",
+            active_index_path="scene.ifc_point_cloud_list_active_index",
+            unique_id="ifc_point_cloud_list_id",
+        )
+
+
+classes = [
+    MyPropGroup,
+    IfcPointCloudPanel,
+]
+
+class_register, class_unregister = bpy.utils.register_classes_factory(classes)
+
+
+def register():
+    class_register()
+    bpy.types.Scene.ifc_point_cloud_list = bpy.props.CollectionProperty(type=MyPropGroup)
+    bpy.types.Scene.ifc_point_cloud_list_active_index = bpy.props.IntProperty()
+
+
+def unregister():
+    class_unregister()
+    del bpy.types.Scene.ifc_point_cloud_list
+    del bpy.types.Scene.ifc_point_cloud_list_active_index
+
+
+register()
+
+bpy.context.scene.ifc_point_cloud_list.clear()
+pcs=IfcPointCloudPanel.find_pcs()
+for item in pcs:
+    list_item = bpy.context.scene.ifc_point_cloud_list.add()
+    list_item.name = item.Description
