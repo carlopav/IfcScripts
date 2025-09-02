833de1af71838ae9e74615f2614baed4bc9383a5
carlopav <carlopav@gmail.com>
2025-09-03 00:26:57 +0200

    updated Ifc PointCloud

--- IFC_StoredPointCloudUI.py
+++ IFC_StoredPointCloudUI.py
@@ -4,6 +4,45 @@ from bl_ui.generic_ui_list import draw_ui_list
 from bonsai import tool
 
 
+bpy.ops.point_cloud_visualizer.add_pcv_helper()
+
+
+class LoadPC(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.load_point_cloud"
+    bl_label = "Load Point Cloud"
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        bpy.context.scene.ifc_point_cloud_list.clear()
+        pcs=IfcPointCloudPanel.find_pcs()
+        for item in pcs:
+            list_item = bpy.context.scene.ifc_point_cloud_list.add()
+            list_item.name = item.Description
+        return {'FINISHED'}
+
+
+class UpdateLinked_PC(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.simple_operator"
+    bl_label = "Update Point Cloud List"
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        bpy.context.scene.ifc_point_cloud_list.clear()
+        pcs=IfcPointCloudPanel.find_pcs()
+        for item in pcs:
+            list_item = bpy.context.scene.ifc_point_cloud_list.add()
+            list_item.name = item.Description
+        return {'FINISHED'}
+
+
 class MyPropGroup(bpy.types.PropertyGroup):
     name: bpy.props.StringProperty()
 
@@ -26,6 +65,16 @@ class IfcPointCloudPanel(bpy.types.Panel):
     
     def draw(self, context):
         layout = self.layout
+        row = layout.row()
+        row.label(text="Update list:")
+        btn_row = row.row(align=True)
+        btn_row.alignment = "RIGHT"
+        btn_row.operator(
+            UpdateLinked_PC.bl_idname, text="", icon='FILE_REFRESH'
+        )
+        btn_row.operator(
+            LoadPC.bl_idname, text="", icon='ADD'
+        )
         draw_ui_list(
             layout,
             context,
@@ -36,6 +85,8 @@ class IfcPointCloudPanel(bpy.types.Panel):
 
 
 classes = [
+    LoadPC,
+    UpdateLinked_PC,
     MyPropGroup,
     IfcPointCloudPanel,
 ]
@@ -57,8 +108,3 @@ def unregister():
 
 register()
 
-bpy.context.scene.ifc_point_cloud_list.clear()
-pcs=IfcPointCloudPanel.find_pcs()
-for item in pcs:
-    list_item = bpy.context.scene.ifc_point_cloud_list.add()
-    list_item.name = item.Description
